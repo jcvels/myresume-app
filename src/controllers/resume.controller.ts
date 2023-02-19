648db2d4e2ea3fe057a52fdfdf4535b7ec257c83
Jorge Pauvels <jcvels@uvcoding.com.ar>
2023-02-19 15:59:32 -0300

    Controller

--- src/controllers/resume.controller.ts
+++ src/controllers/resume.controller.ts
@@ -1,7 +1,6 @@
-export const showResume = (req, res) => {
-  res.render('pages/resume')
-}
+import { Request, Response } from 'express';
+import * as resumeData from '../data/resume-data.json'
 
-export const setResume = () => {
-  return false
+export const showResume = (_req:Request, res:Response) => {
+  res.render('pages/resume', resumeData)
 }
