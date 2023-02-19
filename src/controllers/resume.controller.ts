8c03c1177c79836b320619f6cada9e4e28723bfe
Jorge Pauvels <jcvels@uvcoding.com.ar>
2023-02-19 20:20:45 -0300

    Change to local tool

--- src/controllers/resume.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import * as resumeData from '../data/resume-data.json'
-
-export const showResume = (_req:Request, res:Response) => {
-  res.render('pages/resume', { data:resumeData })
-}
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { app } from './app'
-
-const port = process.env.PORT || 8080;
-
-app.listen(port)
-  .on('listening', () => console.log(`--> listening port ${port}.`) )
-  .on('request', (data) => console.log(`--> new ${data.method} request to ${data.url}` ) )
-  .on('error', (err) => console.log(`--> ${err}`) )
