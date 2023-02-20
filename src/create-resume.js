f65f9f81b70c52ba6286f575f1f60ca9134bf965
Jorge Pauvels <jcvels@uvcoding.com.ar>
2023-02-19 21:27:20 -0300

    Create CLI

--- src/create-resume.js
+++ src/create-resume.js
@@ -1,1 +1,28 @@
-const pug = require('pug');
+const pug = require('pug')
+const path = require('path')
+const fs = require('fs')
+
+const resumeTemplate = path.join( __dirname, './views/resume-template.pug' )
+const templateCompiler = pug.compileFile(resumeTemplate);
+
+try {
+
+  console.log('> Start resume creation')
+
+  const resumeData = require('./data/resume-data.json')
+  const htmlContent = templateCompiler( { data : resumeData } )
+  
+  fs.copyFileSync( path.join( __dirname, './assets/custom.css' ), path.join( __dirname, './tmp/custom.css' )  )
+  fs.copyFileSync( path.join( __dirname, './assets/picture.jpg' ), path.join( __dirname, './tmp/picture.jpg' )  )
+  fs.writeFileSync( path.join( __dirname, './tmp/index.html' ), htmlContent )
+  
+  console.log('> Resume successfuly created')
+
+} catch (error) {
+
+  console.error(`> ERROR: ${error}\n`)
+  process.exit()
+
+}
+
+console.log('> myresume-app v1.0.0\n')
